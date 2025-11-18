dbc3e27c5407f890b12cb54fd7dd867e230a9458
shreyashkushwaha21 <shreyash.kushwaha.cs28@iilm.edu>
2025-11-18 11:20:56 +0530

    Create arthemetic operations.java

---
+++ arthemetic operations.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//03 perform arithmetic operations
+public class Arithm {
+    // perform arithmetic operations
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+
+
+
+    }
+
+}
